53e8ac0db8f24ec0ecf0b24f702a32e2323a40d7
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2022-12-08 10:36:01 -0300

    primeiro exercicio da lista

---
+++ src/primeiroProjeto/primeiraClasse.java
@@ -0,0 +1,29 @@
+package primeiroProjeto;
+
+import java.util.Scanner;
+
+public class primeiraClasse {
+
+	public static void main(String[] args) {
+		// declaração de variaveis
+		int ano, mes, dia, somadias;
+		// declaração de uso do scanner, para pegar dados de entrada do usuário
+		Scanner entrada = new Scanner(System.in);
+		
+		// interação com o usuario
+		System.out.println("Anos de vida: ");
+		// a variável entrada será preenchida com o proximo inteiro digitado no terminal
+		// e depois será repassada para a variavel ano
+		ano = entrada.nextInt();
+		
+		System.out.println("Mes de nascimento: ");
+		mes = entrada.nextInt();
+		
+		System.out.println("Dia de nascimento: ");
+		dia = entrada.nextInt();
+		
+		somadias = (((mes*31)+ dia)+(ano*365));
+		System.out.println("Sua idade em dias eh: " + somadias);
+	}
+
+}
